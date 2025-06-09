5f318dbcee614b51054baeb559952f7a89d93126
Zaheer <abdulzaheer963@gmail.com>
2025-06-09 12:55:19 +0530

    SI cal

---
+++ Simple interest.py
@@ -0,0 +1,5 @@
+p=float(input("Enter Principle amount : "))
+n=float(input("Enter time period : "))
+r=float(input("Enter interest rate : "))
+Si=p*n*r/100
+print("Simple intrest of given values =",Si,"")
