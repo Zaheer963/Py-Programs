d47148b97772fe8d993d1027824861d2b5868c54
Zaheer <abdulzaheer963@gmail.com>
2025-06-09 12:43:16 +0530

    sum

---
+++ Sum of even numbers.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the number : "))
+sum=0
+b=0
+while b<=a:
+    sum+=b
+    b=b+2
+print(sum)
---
+++ Sum of odd numbers.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the number : "))
+sum=0
+b=0
+while b<=a:
+    if b%2>=1:
+        sum+=b
+    b+=1
+print(sum)
